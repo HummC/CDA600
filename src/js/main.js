d66939b308ecc889b28f64755af27d7115e95f7d
HummC <2hummc15@solent.ac.uk>
2018-04-18 14:45:59 +0100

    Added bootstrap + organised vue component CSS into separate SCSS files
    Consider changing similiar PHP files like showgoal and showtasks to a single file named SHOW and differentiate based on goalid, taskid etc.

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './components/App.vue'
 
-import '../index.php'
+import '../login.html'
+import '../home.php'
 import '../scss/main.scss'
+
 
 new Vue({
   el: '#app',
