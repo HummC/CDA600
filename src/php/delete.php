bb25e438e9ab951dbe81dc3938e1c652b3de14a1
HummC <2hummc15@solent.ac.uk>
2018-04-21 23:13:25 +0100

    Delete.php script completed!
    I've corrected the delete script so that data with foreign constraints is deleted before the parent, so if a goal or group is deleted then the group members or tasks will be deleted first

--- src/php/delete.php
+++ src/php/delete.php
@@ -11,8 +11,9 @@ else {
     require('connect.php');
     
     
-    // DELETING GOALS
+    // DELETING GOALS + TASKS FOR THAT GOAL
 if(isset($_POST['goal_id'])) {
+       echo "I am inside isset goal id";
        $goal_id = $_POST['goal_id'];
        $sql = "DELETE FROM goals WHERE userID=:userid AND ID=:goalid";
        $statement = $conn->prepare($sql);
@@ -20,22 +21,56 @@ if(isset($_POST['goal_id'])) {
            array(':userid'=>$user_id,
                  ':goalid'=>$goal_id
                 ));
-    header("HTTP/1.1 200 OK");
+    
+    
+      // THEN DELETE THE TASKS
+       echo "DELETING groups has ran sucessfully";
+       $sql = "DELETE FROM tasks WHERE userID = :userid AND goalID=:goalid";
+       $statement = $conn->prepare($sql);
+       $statement->execute(
+           array(':userid'=>$user_id,
+                 ':goalid'=>$goal_id
+                ));
+        header("HTTP/1.1 200 OK");  
 }
-    // DELETING GROUPS
+    // DELETING GROUPS + TASKS FOR THAT GROUP
     else if(isset($_POST['group_id'])) {
        $group_id = $_POST['group_id'];
-       $sql = "DELETE FROM groups WHERE ownerID=:userid AND ID=:groupid";
+       
+        // DELETE THE MEMBERS OF THE GROUP FIRST - FOREIGN KEY CONSTRAINT
+       $sql = "DELETE FROM usergroup WHERE groupID=:groupid";
+       $statement = $conn->prepare($sql);
+       $statement->execute(
+           array(
+                 ':groupid'=>$group_id
+                ));
+        
+        
+        // THEN DELETE THE GROUP
+       echo "I am inside isset group id";
+       $sql = "DELETE FROM groups WHERE ownerID = :userid AND ID=:groupid";
        $statement = $conn->prepare($sql);
        $statement->execute(
            array(':userid'=>$user_id,
                  ':groupid'=>$group_id
                 ));
-        header("HTTP/1.1 200 OK");
+        
+        
+       // THEN DELETE THE TASKS
+       echo "DELETING groups has ran sucessfully";
+       $sql = "DELETE FROM tasks WHERE userID = :userid AND groupID=:groupid";
+       $statement = $conn->prepare($sql);
+       $statement->execute(
+           array(':userid'=>$user_id,
+                 ':groupid'=>$group_id
+                ));
+        header("HTTP/1.1 200 OK"); 
+      
     }
     
     // DELETING TASKS
     else if(isset($_POST['task_id'])) {
+       echo "I am inside isset task id";
        $group_id = $_POST['task_id'];
        $sql = "DELETE FROM tasks WHERE userID=:userid AND ID=:taskid";
        $statement = $conn->prepare($sql);
