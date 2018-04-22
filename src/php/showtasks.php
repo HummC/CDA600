085e4464c9f86f7205c02cfb03f37915428bc853
HummC <2hummc15@solent.ac.uk>
2018-04-22 11:29:20 +0100

    showtasks.php coded - 1 issue
    Shows tasks for user ID or task ID, but doesn't retrieve information on either user or parent group/goal. Need to create a join query to store profile name and parent goal / group name. Currently this is secondary priority to completing the rest of the scripts and building the front-end. I will re-visit it as the UI is built and make changes if necessary, much of the information may already be received through other API calls since it is a single page app and props can cascade information for re-use

---
+++ src/php/showtasks.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+session_regenerate_id();
+header('content-type: application/json');
+if (!isset($_SESSION["user_id"])) {
+    header("Location: ../login.html");
+    die();
+}
+
+else {
+    $user_id = $_SESSION['user_id'];
+    require('connect.php');
+    
+    if(isset($_GET['mytasks'])) {
+       $sql = "SELECT * FROM tasks WHERE userID=:userid";
+       $statement = $conn->prepare($sql);
+       $statement->execute(
+           array(':userid'=>$user_id
+                ));
+       $row = $statement->fetchAll(PDO::FETCH_ASSOC);
+       if(!$row) {
+                echo "You have no tasks";
+                header("HTTP/1.1 404 Not Found");
+                die();
+            }
+            else { 
+                echo json_encode($row);
+                header("HTTP/1.1 200 OK");
+                
+            }
+    }
+    
+    // For single group pages, we need a way to pass back the information
+    // Axios will show groups for the owner or all groups. The group id will
+    // be stored by the event triggered and passed via route parameters to
+    // the group-single component. Using the created life-cycle hook, this script
+    // will then be requested again passing the group_id as a query parameter. 
+    // Information for that single group will then be returned and mounted using
+    // Vue.
+    
+    
+    // If we are looking at the single group page, then we also need information on group members
+    // aswell as the group itself. As such as will need to make two additional queries. One to
+    // link members to the group through the usergroups table and one to link user information to
+    // to the userID through the users table.
+    
+    else if(isset($_GET['task_id'])) {
+       $taskid = htmlspecialchars($_GET['task_id']);
+       $sql = "SELECT * FROM tasks WHERE ID=:taskid";
+       $statement = $conn->prepare($sql);
+       $statement->execute(
+           array(':taskid'=>$taskid
+                ));
+       $row = $statement->fetchAll(PDO::FETCH_ASSOC);
+       if(!$row) {
+                echo "You have no tasks!";
+                header("HTTP/1.1 404 Not Found");
+                die();
+            }
+            else { 
+                echo json_encode($row);
+                header("HTTP/1.1 200 OK");
+                
+            }
+     
+    }
+    
+    else {
+        header("HTTP/1.1 404 Not Found");
+        die();
+    }
+}
+
+?>
