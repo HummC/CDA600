c3a8302024d5469c5fa3801f77027563d097f2a8
HummC <2hummc15@solent.ac.uk>
2018-04-22 15:49:57 +0100

    showtasks works perfectly
    Task owners can view their tasks, but non owners cannot. However, if that tasks parent is a group, then other group members can view that task. This basic logic dictates the difference between private / public in my application, separting tasks into group tasks and personal tasks. Both require a degree of authentication to view, whether that be matching userid to the currently logged in user or a matching record in the usergroups table.

--- src/php/showtasks.php
+++ src/php/showtasks.php
@@ -10,22 +10,7 @@ if (!isset($_SESSION["user_id"])) {
 else {
     $user_id = $_SESSION['user_id'];
     require('connect.php');
-    
-    
-    // For single group pages, we need a way to pass back the information
-    // Axios will show groups for the owner or all groups. The group id will
-    // be stored by the event triggered and passed via route parameters to
-    // the group-single component. Using the created life-cycle hook, this script
-    // will then be requested again passing the group_id as a query parameter. 
-    // Information for that single group will then be returned and mounted using
-    // Vue.
-    
-    
-    // If we are looking at the single group page, then we also need information on group members
-    // aswell as the group itself. As such as will need to make two additional queries. One to
-    // link members to the group through the usergroups table and one to link user information to
-    // to the userID through the users table.
-    
+        
      if(isset($_GET['task_id'])) {
     // determine if group members
       $sql = "SELECT t.userID, g.groupID, g.userID FROM tasks AS t, usergroup AS g WHERE g.groupID=t.groupID AND g.userID = :userid OR t.userID = :userid";
