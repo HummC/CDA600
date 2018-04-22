a568cffdd65f0bb351fd11996a9a975cc588ad2b
HummC <2hummc15@solent.ac.uk>
2018-04-22 22:23:41 +0100

    current progress - complete.php

--- src/php/complete.php
+++ src/php/complete.php
@@ -1,30 +1,99 @@
-
+<?php
+session_start();
+session_regenerate_id();
 // CHECK THAT USER IS AUTHORISED TO RUN THIS SCRIPT
+if (!isset($_SESSION["user_id"])) {
+    header("Location: ../login.html");
+    die();
+}
+else {
+// CHECK WHICH OUT OF GOAL, TASK AND GROUP IS SET   
+if(isset($_POST['group_id']) OR isset($_POST['goal_id']) OR isset($_POST['task_id'])) {
+    if(isset($_POST['group_id'])) {
+        $group_id = $_POST['group_id'];
+        if(isset($_POST['complete'])) {
+            $status = $_POST['complete'];
+            if($status == 1) {
+                $status = "complete";
+            }
+            else {
+                $status = "in-complete";
+            }
+            // UPDATE QUERY FOR GROUP
+           $sql = "UPDATE `groups` SET status=:status WHERE ID=:groupid AND ownerID=:userid";
+           $statement = $conn->prepare($sql);
+           $statement->execute(
+           array(':groupid'=>$group_id,
+                 ':userid'=>$user_id
+                ));
+           header("HTTP/1.1 200 OK"); 
+        }
+        else {
+            // BAD REQUEST
+            header("HTTP/1.1 400 Bad request");
+            die();
+        }
+    }
+    else if(isset($_POST['goal_id'])) {
+        $goal_id = $_POST['goal_id'];
+        if(isset($_POST['complete'])) {
+            $status = $_POST['complete'];
+            if($status == 1) {
+                $status = "complete";
+            }
+            else {
+                $status = "in-complete";
+            }
+            
+            // UPDATE QUERY FOR GOAL
+           $sql = "UPDATE `goals` SET status=:status WHERE ID=:goalid AND userID=:userid";
+           $statement = $conn->prepare($sql);
+           $statement->execute(
+           array(':goalid'=>$goal_id,
+                 ':userid'=>$user_id
+                ));
+           header("HTTP/1.1 200 OK"); 
+        }
+        else {
+            // BAD REQUEST
+            header("HTTP/1.1 400 Bad request");
+            die();
+        }
+    }
+    else if(isset($_POST['task_id'])) {
+        $task_id = $_POST['task_id'];
+        if(isset($_POST['complete'])) {
+            $status = $_POST['complete'];
+            if($status == 1) {
+                $status = "complete";
+            }
+            else {
+                $status = "in-complete";
+            }
+            // UPDATE QUERY FOR GOAL
+            $sql = "UPDATE `tasks` SET status=:status WHERE ID=:taskid AND userID=:userid";
+           $statement = $conn->prepare($sql);
+           $statement->execute(
+           array(':taskid'=>$task_id,
+                 ':userid'=>$user_id
+                ));
+           header("HTTP/1.1 200 OK"); 
+        }
+        else {
+            // BAD REQUEST
+            header("HTTP/1.1 400 Bad request");
+            die();
+        }
+        
+    }
+    else {
+        // INVALID REQUEST
+        header("HTTP/1.1 400 Bad request");
+        die();
+    }
+}   
+    
+}
 
 
 
-
-// CHECK WHICH OUT OF GOAL, TASK AND GROUP IS SET
-
-
-
-// READ INTO VARIABLES
-
-
-
-
-// CHECK IF COMPLETE IS EQUAL TO 1 OR 0
-
-
-
-
-// IF COMPLETE EQUAL TO 1 THEN UPDATE STATUS TO COMPLETE
-
-
-
-
-// ELSE IF COMPLETE EQUAL TO 0 THEN UPDATE STATUS TO IN-COMPLETE
-
-
-
-// ELSE INVALID REQUEST
