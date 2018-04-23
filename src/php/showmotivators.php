b3ca1c79f6764c2ef9580bbd1c8c0dc3bcc85418
HummC <2hummc15@solent.ac.uk>
2018-04-23 12:51:03 +0100

    Finished back-end development, starting front-end

--- src/php/showmotivators.php
+++ src/php/showmotivators.php
@@ -2,6 +2,7 @@
 <?php
 session_start();
 session_regenerate_id();
+header('content-type: application/json');
 // CHECK THAT USER IS AUTHORISED TO RUN THIS SCRIPT
 if (!isset($_SESSION["user_id"])) {
     header("Location: ../login.html");
@@ -9,15 +10,22 @@ if (!isset($_SESSION["user_id"])) {
 }
 
 else {
+    require('connect.php');
     if(isset($_GET['task_id'])) {
+    $taskid = $_GET['task_id'];
+    
+    // SELECT USERS WHO MOTIVATED A PARTICULAR TASK AND RETURN AS JSON TO BE USED IN WEBPAGE.
+    $sqltwo = "SELECT u.image_loc, u.name, u.id, tm.taskID, tm.userID FROM users AS u, taskmotivator AS tm WHERE tm.taskID=:taskid AND tm.userID = u.ID";
+    $statement = $conn->prepare($sqltwo);
+    $statement->execute(
+    array(':taskid'=>$taskid
+                ));
+    $rowtwo = $statement->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($rowtwo); 
         
-    // SELECT FROM taskmotivators where taskid = task id.
-        
-    // STORE USER IDS IN ARRAY
-        
-    // SELECT FROM users WHERE ID IN myarray
-        
-    // JSON ENCODE ROWS
            
+    }
+    else {
+        // NOT SET DISPLAY NOTHING
     }
 }
