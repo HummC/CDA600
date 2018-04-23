8a8f141e4a9bda75fcf84b4527b5e10a0d01c71d
HummC <2hummc15@solent.ac.uk>
2018-04-23 11:28:22 +0100

    Outline for showing motivators
    Last back-end script before starting to work with Front-end

--- src/php/showmotivators.php
+++ src/php/showmotivators.php
@@ -1,22 +1,23 @@
-// Check if user logged in
 
+<?php
+session_start();
+session_regenerate_id();
+// CHECK THAT USER IS AUTHORISED TO RUN THIS SCRIPT
+if (!isset($_SESSION["user_id"])) {
+    header("Location: ../login.html");
+    die();
+}
 
-
-
-// check if task id is set
-
-
-
-
-// check if userid and taskid are present in the taskmotivators table
-
-
-
-
-// select image, name from users table where the userid is equal to the userid from the select query
-
-
-
-
-
-// if they are remove userid and taskid from motivators table and update task table to motivates - 1
+else {
+    if(isset($_GET['task_id'])) {
+        
+    // SELECT FROM taskmotivators where taskid = task id.
+        
+    // STORE USER IDS IN ARRAY
+        
+    // SELECT FROM users WHERE ID IN myarray
+        
+    // JSON ENCODE ROWS
+           
+    }
+}
