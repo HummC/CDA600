314783c994cb2080272c3c714b554b0a744e9d06
HummC <2hummc15@solent.ac.uk>
2018-04-24 19:08:04 +0100

    Routes sorted, need to add links

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,9 +1,18 @@
 import pageProfile from './components/profile/page-profile.vue';
 import pageGoals from './components/goals/page-goals.vue';
+import pageTasks from './components/tasks/page-tasks.vue';
+import pageCallendar from './components/callendar/page-callendar.vue';
+import pageGroups from './components/groups/page-groups.vue';
 
 export default[
     {path:"/",
     component:pageProfile},
-    {path:"/#/goals",
-    component: pageGoals}
+    {path:"/goals",
+    component: pageGoals},
+    {path:"/tasks",
+    component: pageTasks},
+    {path:"/callendar",
+    component: pageCallendar},
+    {path:"/groups",
+    component: pageGroups}
 ]
